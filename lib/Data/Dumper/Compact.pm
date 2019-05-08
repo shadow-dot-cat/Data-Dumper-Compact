eca0cacb6b5f0448a0add0de87683708b72defe4
Matt S Trout <mst@shadowcat.co.uk>
2019-05-08 00:07:54 +0000

    redo transform setup, add some docs

--- lib/Data/Dumper/Compact.pm
+++ lib/Data/Dumper/Compact.pm
@@ -5,9 +5,18 @@ use Data::Dumper ();
 use Mu;
 use namespace::clean;
 
+sub import {
+  my ($class, $ddc, $opts) = @_;
+  return unless ($ddc||'') eq 'ddc';
+  my $targ = caller;
+  my $cb = $class->new($opts||{})->dump_cb;
+  no strict 'refs';
+  *{"${targ}::ddc"} = $cb;
+}
+
 ro max_width => default => 78;
 
-lazy width => sub { shift->max_width };
+lazy width => sub { shift->max_width }, init_arg => undef;
 
 lazy indent_width => sub { length($_[0]->indent_by) };
 
@@ -15,7 +24,7 @@ sub _next_width { $_[0]->width - $_[0]->indent_width }
 
 ro indent_by => default => '  ';
 
-rw transforms => default => sub { [] };
+ro transforms => default => sub { [] };
 
 sub add_transform { push(@{$_[0]->transforms}, $_[1]); $_[0] }
 
@@ -47,10 +56,20 @@ lazy dumper => sub {
 sub _dumper { $_[0]->dumper->($_[1]) }
 
 sub dump {
-  my ($self, $to_dump, $options) = @_;
-  ref($self) and local @{$self}{keys %{$options||{}}} = values %{$options||{}};
-  ref($self) or $self = $self->new($options||{});
+  my ($self, $to_dump, $opts) = @_;
+  # if we're an object, localize anything provided in the options,
+  # and also blow away the dependent attributes if indent_by is changed
+  ref($self) and $opts
+    and (local @{$self}{keys %$opts} = values %$opts, 1)
+    and $opts->{indent_by}
+    and delete @{$self}{grep !$opts->{$_}, qw(indent_width dumper)};
+  ref($self) or $self = $self->new($opts||{});
   $self->format($self->transform($self->transforms, $self->expand($to_dump)));
+}
+
+sub dump_cb {
+  my ($self) = @_;
+  return sub { $self->dump(@_) };
 }
 
 sub expand {
@@ -73,36 +92,35 @@ sub expand {
 sub transform {
   my ($self, $tfspec, $data) = @_;
   return $data unless $tfspec;
-  $self->_transform($tfspec, $data, []);
+  local $self->{transforms} = $tfspec;
+  $self->_transform($data, []);
 }
 
 sub _transform {
-  my ($self, $tfspec, $data, $path) = @_;
-  my $type = $data->[0];
+  my ($self, $data, $path) = @_;
+  my ($type, $payload) = @$data;
   if ($type eq 'hash') {
-    my %h = %{$data->[1][1]};
-    $data = [
-      $data->[0],
-      [
-        $data->[1][0],
-        +{ map +(
-             $_ => $self->_transform($tfspec, $h{$_}, [ @$path, $_ ])
-          ), keys %h
-        },
-      ]
+    my %h = %{$payload->[1]};
+    $payload = [
+      $payload->[0],
+      { map +(
+          $_ => $self->_transform($h{$_}, [ @$path, $_ ])
+        ), keys %h
+      },
     ];
   } elsif ($type eq 'array') {
-    my @a = @{$data->[1]};
-    $data = [
-      $data->[0],
-      [ map $self->_transform($tfspec, $a[$_], [ @$path, $_ ]), 0..$#a ]
-    ];
+    my @a = @$payload;
+    $payload = [ map $self->_transform($a[$_], [ @$path, $_ ]), 0..$#a ];
   }
+  my $tfspec = $self->transforms;
   foreach my $tf (ref($tfspec) eq 'ARRAY' ? @$tfspec : $tfspec) {
     next unless my $cb = ref($tf) eq 'HASH' ? $tf->{$type}||$tf->{_} : $tf;
-    $data = $self->$cb($tfspec, $data, $path);
+    ($type, $payload) = @{
+      $self->$cb($type, $payload, $path)
+      || [ $type, $payload ]
+    };
   }
-  return $data;
+  return [ $type, $payload ];
 }
 
 sub format {
@@ -280,8 +298,143 @@ sub _format_single {
 
 1;
 
-=head1 ALGORITHM
+=head1 NAME
 
+Data::Dumper::Compact - Vertically compact width-limited data formatter
 
+=head1 SYNOPSIS
+
+Basic usage as a function:
+
+  use Data::Dumper::Compact 'ddc';
+  
+  warn ddc($some_data_structure);
+  
+  warn ddc($some_data_structure, \%options);
+
+Slightly more clever usage as a function:
+
+  use Data::Dumper::Compact ddc => \%default_options;
+  
+  warn ddc($some_data_structure);
+  
+  warn ddc($some_data_structure, \%extra_options);
+
+OO usage:
+
+  use Data::Dumper::Compact;
+  
+  warn Data::Dumper::Compact->dump($data, \%options);
+  
+  my $ddc = Data::Dumper::Compact->new(\%options);
+  
+  warn $ddc->dump($data);
+  
+  warn $ddc->dump($data, \%extra_options);
+
+=head1 OPTIONS
+
+=head2 max_width
+
+Represents the width that DDC will attempt to keep as the maximum (if
+something overflows it in spite of our best efforts, DDC will fall back to
+a more vertically sprawling format to at least overflow as little as
+feasible).
+
+Default: C<78>
+
+=head2 indent_by
+
+The string to indent by. To set e.g. 4 space indent, pass C<' 'x4>.
+
+Default: C<'  '>
+
+=head2 indent_width
+
+How many characters one indent should be considered to be. Generally you
+only need to manually set this if your L</ident_by> is C<"\t">.
+
+Default: C<length($self->indent_by)>
+
+=head2 transforms
+
+Set of transforms to apply on every L</dump> operation. See L</transform>
+for more information.
+
+Default: C<[]>
+
+=head2 dumper
+
+The dumper function to be used for dumping things DDC doesn't understand,
+such as coderefs, regexprefs, etc.
+
+Defaults to the same options as L<Data::Dumper::Concise> with a little bit
+of extra cleverness to make L<B::Deparse> use the correct indentation, since
+for some reason L<Data::Dumper> doesn't (at the time of writing) do that.
+
+If you supply it yourself, it needs to be a single argument coderef - you
+could for example use C<\&Data::Dumper::Dumper> though that would almost
+certainly be pointless.
+
+=head1 EXPORTS
+
+=head2 ddc
+
+  use Data::Dumper::Compact 'ddc';
+  use Data::Dumper::Compact 'ddc' => \%options;
+
+If the first argument to C<use>/C<import()> is 'ddc', a subroutine C<ddc()>
+is installed in the calling package which behaves like calling L</dump>.
+
+If the second argument is a hashref, it becomes the options passed to L</new>.
+
+This function is not exported by default.
+
+=head1 METHODS
+
+=head2 new
+
+  Data::Dumper::Concise->new;
+  Data::Dumper::Concise->new(%options);
+  Data::Dumper::Concise->new(\%options);
+
+Constructor. Takes a hash or hashref of L</OPTIONS>
+
+=head2 dump
+
+  Data::Dumper::Concise->dump($data, \%options?);
+  
+  $ddc->dump($data, \%merge_options?);
+
+This is the method you're going to want to call most of the time, and ties
+together the rest of the functionality into a single data-structure-to-string
+bundle. With just a data argument, it's equivalent to:
+
+  $ddc->format( $ddc->transform( $ddc->transforms, $ddc->expand($data) );
+
+In class method form, options provided are passed to L</new>; in instance
+method form, options if provided are merged into C<$ddc> just for this
+invocation.
+
+=head2 dump_cb
+
+Returns a subroutine reference that's a curried call to L</dump>:
+
+  my $cb = $ddc->dump_cb;
+  
+  $cb->($data, \%extra_options); # equivalent to $ddc->dump(...)
+
+Mostly useful for if you want to create a custom C<ddc()> like thing:
+
+  use Data::Dumper::Compact;
+  BEGIN { *Dumper = Data::Dumper::Compact->new->dump_cb }
+
+=head2 expand
+
+=head2 transform
+
+=head2 add_transform
+
+=head2 format
 
 =cut
